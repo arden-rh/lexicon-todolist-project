57b0e8589808451560e92ad210a367670d803a80
Arden <arden.haldorson@gmail.com>
2025-12-08 16:05:50 +0100

    start working on the user interface

--- TodoListProject/Program.cs
+++ TodoListProject/Program.cs
@@ -7,3 +7,18 @@ TodoList ListOfTodos = new TodoList();
 int IncompleteTodos = ListOfTodos.GetIncompleteTodos().Count;
 int CompletedTodos = ListOfTodos.GetCompletedTodos().Count;
 
+
+do
+{
+    Console.WriteLine("Welcome to the Todo List Application!");
+    Console.WriteLine($"You have {IncompleteTodos} tasks to do and {CompletedTodos} completed tasks.");
+    // Menu
+    Console.WriteLine("Pick an option:");
+    Console.WriteLine("(1) Show Todo List (by date or project");
+    Console.WriteLine("(2) Add New Todo");
+    Console.WriteLine("(3) Edit Todo (update, mark as completed, remove)");
+    Console.WriteLine("(4) Save and Quit");
+    break;
+
+}
+while (true);
---
+++ TodoListProject/Utilities.cs
@@ -0,0 +1,32 @@
+﻿/* Utility methods */
+
+namespace TodoListProject
+{
+    internal class Utilities
+    {
+        // NOTE TO SELF: Look at this later
+        public static void WriteColoredKey(string text, string key, ConsoleColor color)
+        {
+            // Find where the key is in the text
+            int index = text.IndexOf(key);
+
+            if (index == -1)
+            {
+                // If the key isn't found, just write the text normally
+                Console.WriteLine(text);
+                return;
+            }
+
+            // 1. Write the part before the key
+            Console.Write(text.Substring(0, index));
+
+            // 2. Change color and write the key
+            Console.ForegroundColor = color;
+            Console.Write(key);
+
+            // 3. Reset color and write the part after the key
+            Console.ResetColor();
+            Console.WriteLine(text.Substring(index + key.Length));
+        }
+    }
+}
