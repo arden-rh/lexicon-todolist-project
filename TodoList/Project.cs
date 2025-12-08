ad798c6b42d450f80015c38db718059d07f693fd
Arden <arden.haldorson@gmail.com>
2025-12-08 11:15:10 +0100

    first commit

---
+++ TodoList/Project.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoList
+{
+    public class Project
+    {
+        public string Name { get; set; }
+        public List<Todo> Todos { get; set; }
+
+
+        public Project(string name)
+        {
+            Name = name;
+            Todos = new List<Todo>();
+        }
+
+        public void AddTodo(Todo todo)
+        {
+            Todos.Add(todo);
+        }
+
+        public void RemoveTodo(Todo todo) {
+            Todos.Remove(todo); 
+        }
+
+
+    }
+}
---
+++ TodoList/Todo.cs
@@ -0,0 +1,12 @@
+﻿/* Todo */
+
+namespace TodoList
+{
+    public class Todo
+    {
+        public string Title { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool IsCompleted { get; set; }
+        public Project Project { get; set; }
+    }
+}
