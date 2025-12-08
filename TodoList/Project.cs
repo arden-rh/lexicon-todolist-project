8924662beab22892544ce663288a0a8029d4c423
Arden <arden.haldorson@gmail.com>
2025-12-08 14:51:30 +0100

    creating some initial methods for creating, removing and editing a todo

--- TodoList/Project.cs
+++ TodoList/Project.cs
@@ -10,20 +10,37 @@ namespace TodoList
         public List<Todo> Todos { get; set; }
 
 
-        public Project(string name)
+        public Project(string name, Todo firstTodo)
         {
+
+            if (firstTodo == null)
+            {
+                throw new ArgumentNullException(nameof(firstTodo), "First todo cannot be null");
+            }
+
             Name = name;
-            Todos = new List<Todo>();
+            Todos = new List<Todo> { firstTodo };
+          
         }
 
+        //
         public void AddTodo(Todo todo)
         {
-            Todos.Add(todo);
+            if (todo != null)
+            {
+                Todos.Add(todo);
+            }
         }
 
+        //
         public void RemoveTodo(Todo todo) {
-            Todos.Remove(todo); 
+            if (todo != null)
+            {
+                Todos.Remove(todo);
+            }
         }
+
+        //
 
 
     }
--- TodoList/Todo.cs
+++ TodoList/Todo.cs
@@ -4,9 +4,39 @@ namespace TodoList
 {
     public class Todo
     {
+        public int Id { get; }
         public string Title { get; set; }
         public DateTime DueDate { get; set; }
         public bool IsCompleted { get; set; }
         public Project Project { get; set; }
+
+        public Todo(string title, DateTime dueDate, Project project)
+        {
+            Id = new Random().Next(1, 100000); // Simple random ID generation
+            Title = title;
+            DueDate = dueDate;
+            IsCompleted = false;
+            Project = project;
+        }
+
+        // Mark the todo as completed
+        public void MarkAsCompleted()
+        {
+            IsCompleted = true;
+        }
+
+        // Edit the todo details
+        public void Edit(
+            string newTitle = null, 
+            DateTime? newDueDate = null,
+            bool? isCompleted = null,
+            Project newProject = null)
+        {
+            // Update only the provided fields
+            Title = newTitle ?? Title;
+            DueDate = newDueDate ?? DueDate;
+            IsCompleted = isCompleted ?? IsCompleted;
+            Project = newProject ?? Project;
+        }
     }
 }
